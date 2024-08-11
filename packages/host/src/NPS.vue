85522d3c7bb1fce383bf970f09717b3b60cd8434
jake <gerion@gmail.com>
2024-08-11 11:13:53 +0900

    라우터 메뉴 변경

--- packages/host/src/NPS.vue
+++ packages/host/src/NPS.vue
@@ -29,15 +29,35 @@ router.addRoute({
 })
 
 
+// 리모트 페이지 컴포넌트 (SPGEQLQ00001) dynamic import
+// @ts-ignore
+const SPGEQLQ00001 = defineAsyncComponent(() => import('ql_q_newbusiness_ui/SPGEQLQ00001'))
+router.addRoute({
+  path: '/SPGEQLQ00001',
+  name: 'SPGEQLQ00001',
+  component: SPGEQLQ00001
+})
+
+// 리모트 페이지 컴포넌트 (SPGEQLQ99999) dynamic import
+// @ts-ignore
+const SPGEQLQ99999 = defineAsyncComponent(() => import('ql_q_newbusiness_ui/SPGEQLQ99999'))
+router.addRoute({
+  path: '/SPGEQLQ99999',
+  name: 'SPGEQLQ99999',
+  component: SPGEQLQ99999
+})
+
 </script>
 
 <template>
   <header>
     <ul>
-      <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/SPGENPS00001">LocalPage</RouterLink></li>
-      <li><RouterLink to="/SPGEFWZ00001">RemotePage1</RouterLink></li>
-      <li><RouterLink to="/SPGEFWZ99999">RemotePage2</RouterLink></li>
+      <li><RouterLink to="/">Local Component</RouterLink></li>
+      <li><RouterLink to="/SPGENPS00001">Local Component Dynamic Import</RouterLink></li>
+      <li><RouterLink to="/SPGEFWZ00001">FWZ - RemotePage1</RouterLink></li>
+      <li><RouterLink to="/SPGEFWZ99999">FWZ - RemotePage2</RouterLink></li>
+      <li><RouterLink to="/SPGEQLQ00001">QLQ - RemotePage1</RouterLink></li>
+      <li><RouterLink to="/SPGEQLQ99999">QLQ - RemotePage2</RouterLink></li>
     </ul>
   </header>
 
